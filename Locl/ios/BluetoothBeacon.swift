bab19e509294a1ccc75e744bc550a051d7f2c69e
Corey Wilson <coreyjwilson@icloud.com>
2015-10-15 13:38:55 -0700

    Starting to implement setMinor method, had to handle an error for stopLoclbeacon

--- Locl/ios/BluetoothBeacon.swift
+++ Locl/ios/BluetoothBeacon.swift
@@ -16,17 +16,17 @@ class BluetoothBeacon: NSObject, CBPeripheralManagerDelegate {
   var setMinor = 0
   
   //Creates the Beacon and starts broadcasting
-  @objc func initLocalBeacon() -> Void {
+  @objc func initLocalBeacon() {
   if localBeacon != nil {
 		stopLocalBeacon()
   }
   
-  let localBeaconUUID = "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5"
-  let localBeaconMajor: CLBeaconMajorValue = 123
-  let localBeaconMinor: CLBeaconMinorValue = 456
+  let localBeaconUUID = "EF8C2C6F-6B73-4467-BCE2-32DFDDCCD5B9"
+  let localBeaconMajor: CLBeaconMajorValue = 0
+  let localBeaconMinor: CLBeaconMinorValue = CLBeaconMinorValue(setMinor)
   
   let uuid = NSUUID(UUIDString: localBeaconUUID)!
-  localBeacon = CLBeaconRegion(proximityUUID: uuid, major: localBeaconMajor, minor: localBeaconMinor, identifier: "Your private identifer here")
+  localBeacon = CLBeaconRegion(proximityUUID: uuid, major: localBeaconMajor, minor: localBeaconMinor, identifier: "Locl")
   
   beaconPeripheralData = localBeacon.peripheralDataWithMeasuredPower(nil)
   peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: nil)
@@ -35,10 +35,51 @@ class BluetoothBeacon: NSObject, CBPeripheralManagerDelegate {
   
   //Stop broadcasting the current beacon
   @objc func stopLocalBeacon() {
+    if (peripheralManager == nil) {
+      return
+    }
     peripheralManager.stopAdvertising()
     peripheralManager = nil
     beaconPeripheralData = nil
     localBeacon = nil
+  }
+  
+  //Set a new minor for the Beacon
+  @objc func setNewMinor(minor: Int) {
+    setMinor = minor;
+  }
+  
+  @objc func setNewMinor(minor: Int, errorCallback failureCallback: RCTResponseSenderBlock, callback successCallback: RCTResponseSenderBlock) {
+    // Check if fileName is nil or empty
+    if (minor == 0) {
+      let resultsDict = [
+        "success": false,
+        "errMsg": "Not an Int"
+      ]
+      // Execute the JavaScript failure callback handler
+      failureCallback([resultsDict])
+      return; // Halt execution of this function
+    }
+    
+      // Success handling
+    else {
+      setMinor = minor
+      // Craft a success return message
+      let resultsDict = [
+        "success" : true,
+        "successMsg": "Minor set to \(setMinor)"
+      ];
+      // Call the JavaScript sucess handler
+      successCallback([resultsDict]);
+    }
+  }
+      
+  
+  
+  
+  //Get current minor for the Beacon
+  @objc func getMinor(errorCallback failureCallback: RCTResponseSenderBlock, callback successCallback: RCTResponseSenderBlock) -> Int {
+    return setMinor;
   }
   
   //Acts as an intermediary between your app and the iOS Bluetooth stack
@@ -48,26 +89,6 @@ class BluetoothBeacon: NSObject, CBPeripheralManagerDelegate {
     } else if peripheral.state == .PoweredOff {
       peripheralManager.stopAdvertising()
     }
-  }
-  
-  //Set a new minor for the Beacon
-  @objc func setNewMinor(minor: Int, errorCallback failureCallback: RCTResponseSenderBlock, callback successCallback: RCTResponseSenderBlock) -> Void {
-    setMinor = minor;
-    
-    //TODO: Setup failure
-    
-    let resultsDict = [
-      "success" : true,
-      "message" : "Minor set to \(setMinor)"
-    ];
-    // Call the JavaScript sucess handler
-    successCallback([resultsDict]);
-  }
-  
-  //Get current minor for the Beacon
-  @objc func getMinor(errorCallback failureCallback: RCTResponseSenderBlock, callback successCallback: RCTResponseSenderBlock) -> Int {
-    //TODO: setup success/failure
-    return setMinor;
   }
 }
 
