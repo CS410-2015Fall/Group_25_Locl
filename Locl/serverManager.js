10dfe5a7e9c1774ebe72c0c468c9be3e2eb71659
Shehryar Awan <GOhDi55mDream@dhcp-128-189-248-186.ubcsecure.wireless.ubc.ca>
2015-11-13 10:54:17 -0800

    added methods to te serverManager.js file

--- Locl/serverManager.js
+++ Locl/serverManager.js
@@ -85,6 +85,75 @@ var serverManager = React.createClass({
 		})
 		.done();
 	},
+        
+//Create User function. 
+//Inputs: fName= First Name(String)    lName= Last Name(String)    pass= Password(String)    
+//output:none
+//note: Null items are allowed. if user doesnot enter just leave fields blank and the database will store as iTem=null.
+        createCustomer: function(fName,lName,pass) {
+        fetch(customerTableURL, {method: "POST", body: JSON.stringify({FirstName:fName, LastName: lName, Password:pass})})
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log("Search user Query -> " + responseData.CustomerID)
+        })
+        .done();
+    },
+        
+//Create Store fucntion. 
+//Inputs: sName= Store Name(String)    sAddress= Store Address(String)    transmitting= True or False(Boolean)
+//        StoreHTMLimg= URL link with Store image(string)
+//output:none
+//note: sName must be unique and cannot be same as others. trans must be true or false cannot be null.
+        createStore: function(sName,sAddress,transmitting,sImage) {
+        fetch(storeTableURL, {method: "POST", body: JSON.stringify({StoreName: sName, Address: sAddress, Transmitting: transmitting, StoreHTMLimg: sImage})})
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log("Search user Query -> " + responseData.StoreID)
+        })
+        .done();
+    },
+        
+//Create CustomerItem fucntion. 
+//Inputs: itemName= Name of preference Item(Integer)       customerID= Existing CustomerID(Integer)
+//output:none
+//note: customerID is a foreign key and must reference existing Customer in Database
+          createCustomerItem: function(custID, itemName) {
+        fetch(customerItemURL, {method: "POST", body: JSON.stringify({CustID: custID, ItemName: itemName})})
+        .then((response) => response.json())
+        .then((responseData) => {
+            console.log("DONE");
+        })
+        .done();
+    }, 
+        
+//Create StoreItem function. 
+//Inputs: name= Store Name(String)  desc= Store Description(String)  sDate= Start Date(String)   eDate= End Date(String)  
+//        qty= Quantity(Integer)   regPrice = Regular Price(Integer)   sPrice= Sale Price(Integer)    sID= StoreID(Integer) 
+//        upc= UPC(Integer)   imgLink= HTTP Image Link(String)
+//output:none
+//note: sID must be and existing value in store table. HTMLimg can be null or set to a specific link.       
+        createStoreItem: function(name,desc,sDate,eDate,qty,regPrice,salePrice,sID,upc,imgLink) {
+        fetch(itemTableURL, {method: "POST", body: JSON.stringify({Name: name, Description:desc,StartDate:sDate,EndDate:eDate,Quantity:qty, RegPrice:regPrice,SalePrice: salePrice, UPC: upc, StoreID:sID,HTMLimg:imgLink})})
+        .then((response) => response.json())
+        .then((responseData) => {
+             console.log("DONE");
+        })
+        .done();
+    },
+        
+//Search from any table with 1 filter function. 
+//Inputs: fval= Filter Value(String)   ftype= Filter Name(String)  ftable= is one of (customerTableURL/itemTableURL/storeTableURL)
+//output: Array of Results based on search. Null if array has no results.
+//note: so if you want to search from table customer table, by first name and lets say a user by the FirstName of nasrin
+//      you would simply enter searchByOneFilter("nasrin","FirstName",customerTableURL)
+        searchByOneFilter: function searchByOneFilter(fval,fname,ftable) {
+        fetch(ftable+searchByStart+fname+searchByMid+fval+searchByEnd, {method: "GET"})
+        .then((response) => response.json())
+        .then((responseData) => {    
+            console.log("CustName:--->"+responseData.record[0].ItemName)
+        })
+        .done();
+    },        
 
 
 });
