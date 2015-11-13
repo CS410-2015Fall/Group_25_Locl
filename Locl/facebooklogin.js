bb5eb9ae31e1107c45c7a0175d1ae7fbc6ff9951
Wilson Tan <weeweedry@live.com>
2015-11-13 09:49:22 -0800

    Added email permissions and check for logged in

--- Locl/facebooklogin.js
+++ Locl/facebooklogin.js
@@ -18,7 +18,8 @@ var {
 var FBSDKLogin = require('react-native-fbsdklogin');
 var {
   FBSDKGraphRequest,
-  FBSDKLoginButton
+  FBSDKLoginButton,
+  FBSDKLoginManager,
 } = FBSDKLogin;
 
 var FBSDKCore = require('react-native-fbsdkcore');
@@ -31,116 +32,18 @@ var {
 var fetchFriends = new FBSDKGraphRequest((error, result) => {
   if (error)
   {
-    console.log("failed");
     console.log("Error: ", error);
   }
   else
   {
-    console.log("passed");
-    //debugger; //breaks execution
     console.log(result);
   }
-}, '/me');
+}, '/me', {fields: { string: 'name,gender,email'} });
 
 FBSDKGraphRequestManager.batchRequests([fetchFriends],
     function() {}, 60);
 
-
-FBSDKAccessToken.getCurrentAccessToken(token =>
-    console.log(token));
-
 var styles = StyleSheet.create({
-	container: {
-		flex            : 1,
-		alignItems      : 'center',
-		backgroundColor : '#F5FCFF',
-		paddingTop      : 30
-	},
-	instructionsText : {
-		fontSize : 20
-	},
-	separator : {
-		borderWidth  : .5,
-		borderColor  : '#AEAEAE',
-		height       : 1,
-		width        : 300,
-		marginBottom : 25
-	},
-	labelContainer : {
-		flexDirection  : 'row',
-		width          : 300
-	},
-	labelText : {
-		paddingRight : 10,
-		fontSize     : 18
-	},
-	textInput : {
-		height      : 26,
-		borderWidth : 0.5,
-		borderColor : '#0f0f0f',
-		padding     : 4,
-		flex        : 1,
-		fontSize    : 13,
-	},
-	buttonContainer : {
-		flexDirection  : 'row',
-		justifyContent : 'center',
-		alignItems     : 'center',
-		marginTop      : 20
-	},
-	touchableHighlight : {
-		marginLeft  : 10,
-		marginRight : 10,
-	},
-	buttonBox : {
-		flexDirection  : 'row',
-		justifyContent : 'center',
-		alignItems     : 'center',
-		padding        : 10,
-		borderWidth    : 2,
-		borderRadius   : 5
-	},
-	saveButtonBox : {
-		borderColor : '#AA0000'
-	},
-	loadButtonBox : {
-		borderColor : '#00AA00'
-	},
-	setButtonBox : {
-		borderColor : '#00AA00'
-	},
-	buttonText : {
-		fontSize : 16,
-	},
-	outputContainer : {
-		width          : 300,
-		height         : 200,
-		justifyContent : 'center',
-		alignItems     : 'center',
-		borderWidth    : .5,
-		borderColor    : "#999999",
-		marginTop      : 20
-	}, 
-	row: {
-    padding: 8,
-    paddingBottom: 16
-  	}, 
-  	smallText: {
-    fontSize: 11
- 	},
- 	     formInput: {
-        flex: 1,
-        height: 26,
-        fontSize: 13,
-        borderWidth: 1,
-        borderColor: "#555555",
-    },
-    saved: {
-        fontSize: 20,
-        textAlign: "center",
-        margin: 10,
-    },
-
   description: {
     color: 'black',
     backgroundColor: 'white',
@@ -149,64 +52,54 @@ var styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-  },
-  flowRight: {
-    flexDirection: 'column',
-    alignItems: 'center',
-    alignSelf: 'stretch'
-  },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    alignSelf: 'center'
-  },
-  button: {
-    height: 20,
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
+    alignItems      : 'center',
+    backgroundColor : '#F5FCFF',
+    paddingTop      : 30
   }
 });
 
-
-var FB = React.createClass({
+class FB extends React.Component {
   render(){
     return (<View style={styles.container}>
         <Text style={styles.description}>
         FaceBook Login
         </Text>
-        <FBSDKLoginButton
-        onLoginFinished={(error, result) => {
-          if (error) {
-            alert('Error logging in.');
-          } else {
-            if (result.isCanceled) {
+        <FBSDKLoginButton 
+        onPress= {() => { 
+          FBSDKLoginManager.logInWithReadPermissions(['email'],
+              (error, result) => {
+                //call back not called. uses onloginfinished instead
+              })}}
+          onLoginFinished={(error, result) => {
+            if (result.isCancelled) {
               alert('Login cancelled.');
             } else {
-              this.loadPreferences();
+              alert('Logged in.');
             }
-          }
-        }}
-        onLogoutFinished={() => alert('Logged out.')}
-        readPermissions={[]}
-        publishPermissions={['publish_actions']}/>
-        </View>
-        );
-  },
+
+          }}
+          onLogoutFinished={() => alert('Logged out.')}
+          />
+          </View>
+          );
+  }
 
   loadPreferences(){
     this.props.navigator.push({
       title: 'Preferences',
       component: Preferences,
     });
-
   }
-});
+
+  static isLoggedIn(callback: (result: boolean) => void) {
+    FBSDKAccessToken.getCurrentAccessToken(token =>
+        {
+          if (token == null) {
+            callback(false);
+          } else {
+            callback(true);
+          }});
+  }
+}
 
 module.exports = FB;
--- Locl/index.ios.js
+++ Locl/index.ios.js
@@ -3,6 +3,11 @@
 var React = require('react-native');
 var login = require('./facebooklogin');
 
+var FBSDKLogin = require('react-native-fbsdklogin');
+var {
+  FBSDKLoginManager,
+} = FBSDKLogin;
+
 var {
   StyleSheet,
   Text,
@@ -17,96 +22,96 @@ var {
 } = React;
 
 var styles = StyleSheet.create({
-	container: {
-		flex            : 1,
-		alignItems      : 'center',
-		backgroundColor : '#F5FCFF',
-		paddingTop      : 30
-	},
-	instructionsText : {
-		fontSize : 20
-	},
-	separator : {
-		borderWidth  : .5,
-		borderColor  : '#AEAEAE',
-		height       : 1,
-		width        : 300,
-		marginBottom : 25
-	},
-	labelContainer : {
-		flexDirection  : 'row',
-		width          : 300
-	},
-	labelText : {
-		paddingRight : 10,
-		fontSize     : 18
-	},
-	textInput : {
-		height      : 26,
-		borderWidth : 0.5,
-		borderColor : '#0f0f0f',
-		padding     : 4,
-		flex        : 1,
-		fontSize    : 13,
-	},
-	buttonContainer : {
-		flexDirection  : 'row',
-		justifyContent : 'center',
-		alignItems     : 'center',
-		marginTop      : 20
-	},
-	touchableHighlight : {
-		marginLeft  : 10,
-		marginRight : 10,
-	},
-	buttonBox : {
-		flexDirection  : 'row',
-		justifyContent : 'center',
-		alignItems     : 'center',
-		padding        : 10,
-		borderWidth    : 2,
-		borderRadius   : 5
-	},
-	saveButtonBox : {
-		borderColor : '#AA0000'
-	},
-	loadButtonBox : {
-		borderColor : '#00AA00'
-	},
-	setButtonBox : {
-		borderColor : '#00AA00'
-	},
-	buttonText : {
-		fontSize : 16,
-	},
-	outputContainer : {
-		width          : 300,
-		height         : 200,
-		justifyContent : 'center',
-		alignItems     : 'center',
-		borderWidth    : .5,
-		borderColor    : "#999999",
-		marginTop      : 20
-	}, 
-	row: {
+  container: {
+    flex            : 1,
+    alignItems      : 'center',
+    backgroundColor : '#F5FCFF',
+    paddingTop      : 30
+  },
+  instructionsText : {
+    fontSize : 20
+  },
+  separator : {
+    borderWidth  : .5,
+    borderColor  : '#AEAEAE',
+    height       : 1,
+    width        : 300,
+    marginBottom : 25
+  },
+  labelContainer : {
+    flexDirection  : 'row',
+    width          : 300
+  },
+  labelText : {
+    paddingRight : 10,
+    fontSize     : 18
+  },
+  textInput : {
+    height      : 26,
+    borderWidth : 0.5,
+    borderColor : '#0f0f0f',
+    padding     : 4,
+    flex        : 1,
+    fontSize    : 13,
+  },
+  buttonContainer : {
+    flexDirection  : 'row',
+    justifyContent : 'center',
+    alignItems     : 'center',
+    marginTop      : 20
+  },
+  touchableHighlight : {
+    marginLeft  : 10,
+    marginRight : 10,
+  },
+  buttonBox : {
+    flexDirection  : 'row',
+    justifyContent : 'center',
+    alignItems     : 'center',
+    padding        : 10,
+    borderWidth    : 2,
+    borderRadius   : 5
+  },
+  saveButtonBox : {
+    borderColor : '#AA0000'
+  },
+  loadButtonBox : {
+    borderColor : '#00AA00'
+  },
+  setButtonBox : {
+    borderColor : '#00AA00'
+  },
+  buttonText : {
+    fontSize : 16,
+  },
+  outputContainer : {
+    width          : 300,
+    height         : 200,
+    justifyContent : 'center',
+    alignItems     : 'center',
+    borderWidth    : .5,
+    borderColor    : "#999999",
+    marginTop      : 20
+  }, 
+  row: {
     padding: 8,
     paddingBottom: 16
-  	}, 
-  	smallText: {
+  }, 
+  smallText: {
     fontSize: 11
- 	},
- 	     formInput: {
-        flex: 1,
-        height: 26,
-        fontSize: 13,
-        borderWidth: 1,
-        borderColor: "#555555",
-    },
-    saved: {
-        fontSize: 20,
-        textAlign: "center",
-        margin: 10,
-    },
+  },
+  formInput: {
+    flex: 1,
+    height: 26,
+    fontSize: 13,
+    borderWidth: 1,
+    borderColor: "#555555",
+  },
+  saved: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
 
   description: {
     color: 'black',
@@ -142,7 +147,6 @@ var styles = StyleSheet.create({
 });
 
 var Locl = React.createClass({
-
   render() {
     return (
         <NavigatorIOS ref='nav'
@@ -153,7 +157,11 @@ var Locl = React.createClass({
         }}/>
         );
   }
+});
 
+//To check if is logged in
+login.isLoggedIn( result => {  
+  console.log(result);
 });
 
 AppRegistry.registerComponent('Locl', () => Locl);
