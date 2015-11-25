b92fda0e6d316bfdcd09b8761fb111772d54e163
Corey Wilson <coreyjwilson@icloud.com>
2015-11-24 23:22:21 -0800

    ItemPage - Better image resolution - Optimized DreamFactory for larger text strings.

--- Locl/ItemPage.js
+++ Locl/ItemPage.js
@@ -1,6 +1,4 @@
 var React = require('react-native');
-
-var cameraManager = require('./cameraManager.js');
 var UIImagePickerManager = require('NativeModules').UIImagePickerManager;
 
 var {
@@ -19,38 +17,12 @@ var {
   AppRegistry
 } = React;
 
-//UIImagePicker
-var options = {
-  title: 'Select Avatar', // specify null or empty string to remove the title
-  cancelButtonTitle: 'Cancel',
-  takePhotoButtonTitle: 'Take Photo...', // specify null or empty string to remove this button
-  chooseFromLibraryButtonTitle: 'Choose from Library...', // specify null or empty string to remove this button
-  maxWidth: 100,
-  maxHeight: 100,
-  quality: 0.2,
-  allowsEditing: false, // Built in iOS functionality to resize/reposition the image
-  noData: false, // Disables the base64 `data` field from being generated (greatly improves performance on large photos)
-  storageOptions: { // if this key is provided, the image will get saved in the documents directory (rather than a temporary directory)
-    skipBackup: true, // image will NOT be backed up to icloud
-    path: 'images' // will save image at /Documents/images rather than the root
-  }
-};
-
-//app Name for DreamFactory
-var loclSQL="?app_name=loclSQL";
-
-//Basic Connection HTMLstrig for Database
-var httpString="http://ec2-54-187-51-38.us-west-2.compute.amazonaws.com/rest/db/";   
-
-//String to connect to Items Table
-var itemTableURL=httpString+"item"+loclSQL; 
-
 var styles = StyleSheet.create({
  container: {
   marginTop: 10,
   flex: 1,
- },
- textField: {
+},
+textField: {
   height: 40, 
   marginBottom: 2, 
   marginLeft: 2, 
@@ -58,122 +30,143 @@ var styles = StyleSheet.create({
   borderColor: 'F5F5F5', 
   borderWidth: 1,
   paddingLeft: 2, 
- },
- textFieldTitle: {
+},
+textFieldTitle: {
   marginBottom: 2, 
   fontSize: 20,
   color: '#656565'
- },
- button: {
-   height: 20,
-   backgroundColor: 'grey',
-   borderColor: 'grey',
-   borderWidth: 1,
-   borderRadius: 8,
-   margin: 5,
-   color: 'white',
-   alignSelf: 'stretch',
-   justifyContent: 'center',
-   fontSize: 15,
-   textAlign: 'center'
- }, 
+},
+button: {
+ height: 20,
+ backgroundColor: 'grey',
+ borderColor: 'grey',
+ borderWidth: 1,
+ borderRadius: 8,
+ margin: 5,
+ color: 'white',
+ alignSelf: 'stretch',
+ justifyContent: 'center',
+ fontSize: 15,
+ textAlign: 'center'
+}, 
+thumb: {
+  width: 200,
+  height: 200,
+  alignSelf: 'center',
+},
 });
 
 var ItemPage = React.createClass({
   render(){
-    var bottom; 
-    this.printState();
+    var top;
+    var bottom;
+    console.log(this.state.otherSource); 
     if (this.state.updateItem) {
       bottom = 
       <View style={styles.container}>
+
       <TouchableHighlight onPress={this.onUpdatePress}>
-                <Text style={styles.button}>Update</Text>
-              </TouchableHighlight>
-              <TouchableHighlight onPress={this.onDeletePress}>
-            <Text style={styles.button}>Delete</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={UIImagePickerManager.showImagePicker}>
-            <Text style={styles.button}>Camera</Text>
-        </TouchableHighlight>
-        </View>;
+      <Text style={styles.button}>Update</Text>
+      </TouchableHighlight>
+
+      <TouchableHighlight onPress={this.onDeletePress}>
+      <Text style={styles.button}>Delete</Text>
+      </TouchableHighlight>
+
+      </View>;
     } else { 
       bottom = 
-     <View style={styles.container}>
-        <TouchableHighlight onPress={this.onAddPress}>
-            <Text style={styles.button}>Add</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this.onCameraPress}>
-            <Text style={styles.button}>Camera</Text>
-        </TouchableHighlight>
-        </View>;
-      }
+      <View style={styles.container}>
+
+      <TouchableHighlight onPress={this.onAddPress}>
+      <Text style={styles.button}>Add</Text>
+      </TouchableHighlight>
+
+      </View>;
+    }
 
     return (
       <ScrollView style={styles.container}>
-        <Text style={styles.textFieldTitle}>Name:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({name: text})}
-            value={this.state.name}
-        />
+      
+      <TouchableHighlight onPress={this.onCameraPress}>
+      <Image source={{uri: this.state.htmlLink}} style={styles.thumb}/>
+      </TouchableHighlight>
 
-        <Text style={styles.textFieldTitle}>UPC:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({upc: text})}
-            value={this.state.upc}
-        />
+      <Text style={styles.textFieldTitle}>Name:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({name: text})}
+      value={this.state.name}
+      />
 
-        <Text style={styles.textFieldTitle}>Quantity:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({quantity: text})}
-            value={this.state.quantity}
-        />
+      <Text style={styles.textFieldTitle}>UPC:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({upc: text})}
+      value={this.state.upc}
+      />
 
-        <Text style={styles.textFieldTitle}>Regular Price:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({regPrice: text})}
-            value={this.state.regPrice}
-        />
+      <Text style={styles.textFieldTitle}>Quantity:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({quantity: text})}
+      value={this.state.quantity}
+      />
 
-        <Text style={styles.textFieldTitle}>Sale Price:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({salePrice: text})}
-            value={this.state.salePrice}
-        />
+      <Text style={styles.textFieldTitle}>Regular Price:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({regPrice: text})}
+      value={this.state.regPrice}
+      />
 
-        <Text style={styles.textFieldTitle}>Description:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({description: text})}
-            value={this.state.description}
-        />
+      <Text style={styles.textFieldTitle}>Sale Price:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({salePrice: text})}
+      value={this.state.salePrice}
+      />
 
-        <Text style={styles.textFieldTitle}>Start Date:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({startDate: text})}
-            value={this.state.startDate}
-        />
+      <Text style={styles.textFieldTitle}>Description:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({description: text})}
+      value={this.state.description}
+      />
 
-        <Text style={styles.textFieldTitle}>End Date:</Text>
-        <TextInput
-            style={styles.textField}
-            onChangeText={(text) => this.setState({endDate: text})}
-            value={this.state.endDate}
-        />
+      <Text style={styles.textFieldTitle}>Start Date:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({startDate: text})}
+      value={this.state.startDate}
+      />
 
-        <Image source={this.state.avatarSource} style={styles.uploadAvatar} />
-        {bottom}
+      <Text style={styles.textFieldTitle}>End Date:</Text>
+      <TextInput
+      style={styles.textField}
+      onChangeText={(text) => this.setState({endDate: text})}
+      value={this.state.endDate}
+      />
+
+      {bottom}
+      
       </ScrollView>
       );
-  },
+},
 
-  getInitialState: function() {
-
+//Props: ItemData:
+  // [ { ItemID: 4,
+  //     Name: 'Nike Shoes',
+  //     UPC: 112233,
+  //     RegPrice: 120,
+  //     SalePrice: 100,
+  //     Description: 'cheap nike shoes',
+  //     StartDate: '11-22-23',
+  //     EndDate: '11-23-23',
+  //     Quantity: 2,
+  //     StoreID: 101,
+  //     HTMLlink: base64 of image
+getInitialState: function() {
     if (this.props.ItemData) {
       return {
         updateItem: true,
@@ -185,8 +178,7 @@ var ItemPage = React.createClass({
         description: this.props.ItemData.Description,
         startDate: this.props.ItemData.StartDate,
         endDate: this.props.ItemData.EndDate,
-        htmlImg: this.props.ItemData.HTMLimg,
-        avatarSource: "",
+        htmlLink: this.props.ItemData.HTMLlink,
       }
     } else {
       return {
@@ -198,27 +190,14 @@ var ItemPage = React.createClass({
         description: "",
         startDate: "",
         endDate: "",
-        htmlImg: "",
-        avatarSource: "",
+        htmlLink: "http://www.indre-reisid.ee/wp-content/themes/envision/lib/images/default-placeholder.png",
       }
     }
   },
-
-  //Inputs:
-  // [ { ItemID: 4,
-  //     Name: 'Nike Shoes',
-  //     UPC: 112233,
-  //     RegPrice: 120,
-  //     SalePrice: 100,
-  //     Description: 'cheap nike shoes',
-  //     StartDate: '11-22-23',
-  //     EndDate: '11-23-23',
-  //     Quantity: 2,
-  //     StoreID: 101,
-  //     HTMLimg: 'http://www.alegoo.com/images05/footwear/shoes-02/014/nike-basketball-shoes-12.jpg' } ] }
-  
-  //Waiting on Sheyrar for a proper Update Function
-  onUpdatePress: function()  {
+onUpdatePress: function()  {
+  if(this.state.name == "" || this.state.startDate == "" || this.state.endDate == "" || this.state.quantity == "" || this.state.regPrice == "" || this.state.salePrice == "") {
+    AlertIOS.alert("Required fields missing");
+  } else {
     fetch("http://ec2-54-187-51-38.us-west-2.compute.amazonaws.com/rest/db/item?app_name=loclSQL&filter=ItemID=" + this.props.ItemData.ItemID, 
       {method: "Put", body: JSON.stringify({
         Quantity: this.state.quantity, 
@@ -228,79 +207,83 @@ var ItemPage = React.createClass({
         SalePrice: this.state.salePrice, 
         StartDate: this.state.startDate, 
         EndDate: this.state.endDate, 
-        HTMLimg: this.state.htmlIMG, 
+        HTMLlink: this.state.htmlLink, 
         Description: this.state.description
       })})
     .then((responseData) => {
-     console.log("Item Updated");
      AlertIOS.alert("Item Updated");
-    })
+   })
     .done()
-    this.props.navigator.pop();
-  },
+    this.props.navigator.pop();}},
 
-  onAddPress: function() {
-    fetch(itemTableURL, {method: "POST", body: JSON.stringify({Name: this.state.name, Description: this.state.description, StartDate: this.state.startDate, EndDate: this.state.endDate, Quantity: this.state.quantity,  RegPrice: this.state.regPrice, SalePrice: this.state.salePrice, UPC: this.state.upc, StoreID:this.props.StoreID, HTMLimg: this.state.htmlImg})})
+onAddPress: function() {
+  if(this.state.name == "" || this.state.startDate == "" || this.state.endDate == "" || this.state.quantity == "" || this.state.regPrice == "" || this.state.salePrice == "") {
+    AlertIOS.alert("Required fields missing");
+  } else {
+    fetch("http://ec2-54-187-51-38.us-west-2.compute.amazonaws.com/rest/db/item?app_name=loclSQL", 
+      {method: "POST", body: JSON.stringify(
+        {Name: this.state.name, 
+          Description: this.state.description, 
+          StartDate: this.state.startDate, 
+          EndDate: this.state.endDate, 
+          Quantity: this.state.quantity,  
+          RegPrice: this.state.regPrice, 
+          SalePrice: this.state.salePrice, 
+          UPC: this.state.upc, 
+          StoreID:this.props.StoreID, 
+          HTMLlink: this.state.htmlLink})})
     .then((response) => response.json())
     .then((responseData) => {
-     console.log("Item Updated");
      AlertIOS.alert("Item Added");
-    })
+   })
     .done()
     this.props.navigator.pop();
-  },
+  }},
 
-  onDeletePress: function() {
+onDeletePress: function() {
     fetch("http://ec2-54-187-51-38.us-west-2.compute.amazonaws.com/rest/db/item?app_name=loclSQL&filter=ItemID="+this.props.ItemData.ItemID, {method: "DELETE"})
     .then((response) => response.json())
     .then((responseData) => {    
-        console.log("Item Deleted");
-        AlertIOS.alert("Item Deleted");
-      })
+      AlertIOS.alert("Item Deleted");
+    })
     .done();
     this.props.navigator.pop();
   },
 
 // The first arg will be the options object for customization, the second is
-// your callback which sends bool: didCancel, object: response.
-//
-// response.data is the base64 encoded image data
-// response.uri is the uri to the local file asset on the device
-// response.isVertical will be true if the image is vertically oriented
-// response.width & response.height give you the image dimensions
+  // your callback which sends bool: didCancel, object: response.
+  // response.data is the base64 encoded image data
+  // response.uri is the uri to the local file asset on the device
+  // response.isVertical will be true if the image is vertically oriented
+  // response.width & response.height give you the image dimensions
 onCameraPress: function() {
+  var options = {
+  title: 'Get Picture of Item', // specify null or empty string to remove the title
+  cancelButtonTitle: 'Cancel',
+  takePhotoButtonTitle: 'Take Photo...', // specify null or empty string to remove this button
+  chooseFromLibraryButtonTitle: 'Choose from Library...', // specify null or empty string to remove this button
+  maxWidth: 720,
+  maxHeight: 1080,
+  quality: 0.9,
+  allowsEditing: true, // Built in iOS functionality to resize/reposition the image
+  noData: false, // Disables the base64 `data` field from being generated (greatly improves performance on large photos)
+  storageOptions: { // if this key is provided, the image will get saved in the documents directory (rather than a temporary directory)
+    skipBackup: true, // image will NOT be backed up to icloud
+    path: 'images' // will save image at /Documents/images rather than the root
+  }}
   UIImagePickerManager.showImagePicker(options, (didCancel, response) => {
-  console.log('Response = ', response);
-
-  if (didCancel) {
-    console.log('User cancelled image picker');
-  }
-  else {
-    if (response.customButton) {
-      console.log('User tapped custom button: ', response.customButton);
+    console.log('Response = ', response);
+    if (didCancel) {
+      console.log('User cancelled image picker');
     }
     else {
-
-      // You can display the image using either:
       var source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
-      // const source = {uri: response.uri.replace('file://', ''), isStatic: true};
-
-      console.log("Source.uri: " + source.uri);
-
       this.setState({
-        htmlImg: 'data:image/jpeg;base64,' + response.data,
-        avatarSource: 'data:image/jpeg;base64,' + response.data,
+        htmlLink: 'data:image/jpeg;base64,' + response.data,
       });
-
     }
   }
-  }
   )
-},
-
-printState: function() {
-  console.log("htmlImg: " + this.state.htmlImg);
-  console.log("avatarSource: " + this.state.avatarSource);      
 },
 
 });
--- Locl/StoreHome.js
+++ Locl/StoreHome.js
@@ -35,20 +35,20 @@ var styles = StyleSheet.create({
  container: {
   marginTop: 30,
   flex: 1,
- },
- resultsContainer: {
-   flex: 2,
- },
- flowRight: {
-   flexDirection: 'column',
-   alignItems: 'center',
-   alignSelf: 'stretch'
- },
- buttonText: {
-   fontSize: 18,
-   color: 'white',
-   alignSelf: 'center'
- }, 
+},
+resultsContainer: {
+ flex: 2,
+},
+flowRight: {
+ flexDirection: 'column',
+ alignItems: 'center',
+ alignSelf: 'stretch'
+},
+buttonText: {
+ fontSize: 18,
+ color: 'white',
+ alignSelf: 'center'
+}, 
 thumb: {
   width: 50,
   height: 50,
@@ -78,76 +78,76 @@ rowContainer: {
   padding: 10
 }, 
 button: {
-   height: 20,
-   backgroundColor: 'grey',
-   borderColor: 'grey',
-   borderWidth: 1,
-   borderRadius: 8,
-   margin: 5,
-   color: 'white',
-   alignSelf: 'stretch',
-   justifyContent: 'center',
-   fontSize: 15,
-   textAlign: 'center'
- }, 
+ height: 20,
+ backgroundColor: 'grey',
+ borderColor: 'grey',
+ borderWidth: 1,
+ borderRadius: 8,
+ margin: 5,
+ color: 'white',
+ alignSelf: 'stretch',
+ justifyContent: 'center',
+ fontSize: 15,
+ textAlign: 'center'
+}, 
 });
 
 var StoreHome = React.createClass({
   render(){
     this.getStoreItems();
     return (
-        <View style ={styles.container}>
-        <SwitchIOS
-          onValueChange={(value) => this.handlePress(value)}
-          onTintColor="#00ff00"
-          style={{marginBottom: 10}}
-          thumbTintColor="#0000ff"
-          tintColor="#ff0000"
-          value={this.state.colorFalseSwitchIsOn} />
-          <SwitchIOS
-          onValueChange={(value) => this.handlePress(value)}
-          onTintColor="#00ff00"
-          thumbTintColor="#0000ff"
-          tintColor="#ff0000"
-          value={this.state.colorTrueSwitchIsOn} />
-        <TouchableHighlight onPress={this.onAddPress}>
-            <Text style={styles.button}>Add Item</Text>
-        </TouchableHighlight>
-        <ScrollView
-        automaticallyAdjustContentInsets={false}
-        onScroll={() => { console.log('Scrolling! Weeeee'); }}
-        scrollEventThrottle={200}
-        style={styles.scrollView}>
-        <View style={styles.resultsContainer}>
-          <ListView
-           dataSource={this.state.dataSource}
-            renderRow={this.renderRow}
-          />
-        </View>
-        </ScrollView>  
-        </View>
-        );
-  },
+      <View style ={styles.container}>
+      <SwitchIOS
+      onValueChange={(value) => this.handlePress(value)}
+      onTintColor="#00ff00"
+      style={{marginBottom: 10}}
+      thumbTintColor="#0000ff"
+      tintColor="#ff0000"
+      value={this.state.colorFalseSwitchIsOn} />
+      <SwitchIOS
+      onValueChange={(value) => this.handlePress(value)}
+      onTintColor="#00ff00"
+      thumbTintColor="#0000ff"
+      tintColor="#ff0000"
+      value={this.state.colorTrueSwitchIsOn} />
+      <TouchableHighlight onPress={this.onAddPress}>
+      <Text style={styles.button}>Add Item</Text>
+      </TouchableHighlight>
+      <ScrollView
+      automaticallyAdjustContentInsets={false}
+      onScroll={() => { console.log('Scrolling! Weeeee'); }}
+      scrollEventThrottle={200}
+      style={styles.scrollView}>
+      <View style={styles.resultsContainer}>
+      <ListView
+      dataSource={this.state.dataSource}
+      renderRow={this.renderRow}
+      />
+      </View>
+      </ScrollView>  
+      </View>
+      );
+},
 
-  handlePress: function(value) {
-    console.log("handling Press");
-    if (this.state.colorTrueSwitchIsOn) {
-      this.setState({colorFalseSwitchIsOn: true, colorTrueSwitchIsOn: false});
-      bluetoothBeaconManager.onBeaconingStopPress();
-      console.log("colorFalseSwitch set to value, bluetooth set to off");
-    } else {
-      this.setState({colorTrueSwitchIsOn: true, colorFalseSwitchIsOn: false});
-      bluetoothBeaconManager.onBeaconingStartPress();
-      AlertIOS.alert("Bluetooth beaconing will deactivate if your phone locks, or the application is not in the foreground. For best performance, disable locking and keep the application active on the screen.");
-      console.log("colorTrueSwitch set to value, bluetooth set to on");
-    }
-  },
+handlePress: function(value) {
+  console.log("handling Press");
+  if (this.state.colorTrueSwitchIsOn) {
+    this.setState({colorFalseSwitchIsOn: true, colorTrueSwitchIsOn: false});
+    bluetoothBeaconManager.onBeaconingStopPress();
+    console.log("colorFalseSwitch set to value, bluetooth set to off");
+  } else {
+    this.setState({colorTrueSwitchIsOn: true, colorFalseSwitchIsOn: false});
+    bluetoothBeaconManager.onBeaconingStartPress();
+    AlertIOS.alert("Bluetooth beaconing will deactivate if your phone locks, or the application is not in the foreground. For best performance, disable locking and keep the application active on the screen.");
+    console.log("colorTrueSwitch set to value, bluetooth set to on");
+  }
+},
 
-  getInitialState: function() {
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    return {
-            colorTrueSwitchIsOn: false,
-            colorFalseSwitchIsOn: true,
+getInitialState: function() {
+  var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  return {
+    colorTrueSwitchIsOn: false,
+    colorFalseSwitchIsOn: true,
 
             //The storeID props need to come from the index.ios.js push (index.ios.js will get Async the stored StoreID, then pass it via props), for now it is being set to 101 temporarily
             // storeID: this.props.StoreID,
@@ -156,78 +156,67 @@ var StoreHome = React.createClass({
             dataSource: ds.cloneWithRows([]),
             currentAppState: AppStateIOS.currentState,
           };
-  },
+        },
 
-  componentWillMount: function() {
-    bluetoothScanningManager.requestAlwaysAuthorization();
-    bluetoothScanningManager.getAuthorizationStatus();
-    this.getStoreItems();
-    AppStateIOS.addEventListener('change', this.handleAppStateChange);
-  },
+        componentWillMount: function() {
+          bluetoothScanningManager.requestAlwaysAuthorization();
+          bluetoothScanningManager.getAuthorizationStatus();
+          this.getStoreItems();
+          AppStateIOS.addEventListener('change', this.handleAppStateChange);
+        },
 
-  getStoreItems: function() {
-    fetch("http://ec2-54-187-51-38.us-west-2.compute.amazonaws.com/rest/db/" + "item" + "?app_name=loclSQL" + "&filter=StoreID=" + this.state.StoreID, {method: "GET"})
-    .then((response) => response.json())
-    .then((responseData) => {    
-      if (responseData.error) {
-        console.log("Error!");
-        console.log(responseData.error);
-      }
-      else {
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(responseData.record),
-        })
-      }
-    })
-    .done();
-  },
+        getStoreItems: function() {
+          fetch("http://ec2-54-187-51-38.us-west-2.compute.amazonaws.com/rest/db/" + "item" + "?app_name=loclSQL" + "&filter=StoreID=" + this.state.StoreID, {method: "GET"})
+          .then((response) => response.json())
+          .then((responseData) => {    
+            if (responseData.error) {
+              console.log("Error!");
+              console.log(responseData.error);
+            }
+            else {
+              this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(responseData.record),
+              })
+            }
+          })
+          .done();
+        },
 
-  renderRow: function(itemData) {
-    return (
-      <TouchableHighlight underlayColor="#AA9999" onPress={() => this.onUpdatePress(itemData)}> 
-      <View style={styles.rowContainer}>
-      <Image style={styles.thumb} source={{ uri: itemData.HTMLimg }} />
-      <View  style={styles.textContainer}>
-      <Text style={styles.price}>${itemData.SalePrice}</Text>
-      <Text style={styles.title} numberOfLines={1}>{itemData.Name}</Text>
-      </View>
-      </View>
-      </TouchableHighlight>
-      );
-  },
+        renderRow: function(itemData) {
+          return (
+            <TouchableHighlight underlayColor="#AA9999" onPress={() => this.onUpdatePress(itemData)}> 
+            <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: itemData.HTMLlink}} />
+            <View  style={styles.textContainer}>
+            <Text style={styles.price}>${itemData.SalePrice}</Text>
+            <Text style={styles.title} numberOfLines={1}>{itemData.Name}</Text>
+            </View>
+            </View>
+            </TouchableHighlight>
+            );
+        },
 
-  handleAppStateChange: function(currentAppState) {
-  // if (currentAppState == "background") {
-  //   bluetoothBeaconManager.onBeaconingStartPress();
-  // }
-  this.setState({currentAppState: AppStateIOS.currentState});
-  console.log("App State: " + AppStateIOS.currentState);
-  },
+        handleAppStateChange: function(currentAppState) {
+          this.setState({currentAppState: AppStateIOS.currentState});
+          console.log("App State: " + AppStateIOS.currentState);
+        },
 
+        onUpdatePress: function(itemData) {
+          this.props.navigator.push({
+            title: itemData.Name,
+            component: ItemPage,
+            passProps: {ItemData: itemData},
+          });
+        },
 
-  onUpdatePress: function(itemData) {
-    this.props.navigator.push({
-      title: itemData.Name,
-      component: ItemPage,
-      passProps: {ItemData: itemData},
-    });
-  },
+        onAddPress: function() {
+          this.props.navigator.push({
+            title: 'New Item',
+            component: ItemPage,
+            passProps: {ItemData: null, StoreID: this.state.StoreID.toString()},
+          });
+        },
 
-  onAddPress: function() {
-    this.props.navigator.push({
-      title: 'New Item',
-      component: ItemPage,
-      passProps: {ItemData: null, StoreID: this.state.StoreID.toString()},
-    });
-  },
-
-  toBeacon(){
-    this.props.navigator.push({
-      title: 'Beacon',
-      component: Beacon
-    });
-  },
-
-});
+      });
 
 module.exports = StoreHome;
